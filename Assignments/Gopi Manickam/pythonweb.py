8de247caf724d5b55611cfaba2e396ce39679586
ArulJyothi1302 <67818016+ArulJyothi1302@users.noreply.github.com>
2022-09-20 19:31:16 +0530

    Update pythonweb.py

--- Assignments/Gopi Manickam/pythonweb.py
+++ Assignments/Gopi Manickam/pythonweb.py
@@ -1,5 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
-@app.route("/")
+@app.route('/')
 def hello_world():
-    return "<p>Hello, World!</p>"
+   return "Hello World"
+if __name__ == '__main__':
+   app.run()
