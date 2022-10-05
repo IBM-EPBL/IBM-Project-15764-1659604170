93f31aea92affa0082e8cb9963c7e9ac8739cd4f
ArulJyothi1302 <67818016+ArulJyothi1302@users.noreply.github.com>
2022-10-05 19:30:53 +0530

    Create Sample4.py

---
+++ Assignments/Arul Jyothi/Sample4.py
@@ -0,0 +1,14 @@
+#collections
+lst = [10,20,30,40,50]
+print(lst)
+print(type(lst))
+
+#tuple
+l=(10,20,30,40,50)
+print(l)
+print(type(l))
+
+#set
+s={100,200,300,400,500}
+print(s)
+print(type(s))
